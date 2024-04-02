41273f671f751fa6f66490a897037bbe929c0874
kalam khann <khannkalam@gmail.com>
2024-04-02 14:23:43 +0530

    layout done

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./pagination.module.css"
+
+const Magination = () => {
+  return (
+    <div className={styles.conatiner}>pagination
+      
+    </div>
+  )
+}
+
+export default Magination
