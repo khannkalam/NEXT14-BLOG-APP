86ba53d594bc707ac3a2f6b4eca44f7af45e4613
kalam khann <khannkalam@gmail.com>
2024-04-07 20:42:02 +0530

    auth is done

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -26,6 +26,7 @@ if(status === "authenticated" ){
        <div className={styles.container}>
       <div className={styles.wrapper}>
         <div className={styles.socialButton}onClick={()=>signIn("google")}>Sign in with Google</div>
+        <div className={styles.socialButton}>Sign in with apple</div>
         <div className={styles.socialButton}>Sign in with Github</div>
         <div className={styles.socialButton}>Sign in with Facebook</div>
       </div>
