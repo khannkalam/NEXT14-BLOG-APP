674237a6e1f773517f06f249ec3a54a2a24444a8
kalam khann <khannkalam@gmail.com>
2024-04-16 17:48:37 +0530

    ready to deploy

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import styles from "./write.module.css";
 import { useEffect, useState } from "react";
-import "react-quill/dist/quill.bubble.css";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import {
@@ -13,10 +12,13 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "@/utlis/firebase";
-import ReactQuill from "react-quill";
+
+import dynamic from "next/dynamic";
 
 const WritePage = () => {
   const { status } = useSession();
+  const ReactQuill = dynamic(()=> import ('react-quill'),{ssr:false});
+
   const router = useRouter();
 
   const [open, setOpen] = useState(false);
