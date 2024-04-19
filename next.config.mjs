b1e9075589ba4562041eb4a96cb8e063fd978902
kalam khann <khannkalam@gmail.com>
2024-04-19 14:49:12 +0530

    ready to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
-  },
+images:{
+    domains:["lh3.googleusercontent.com", "firebasestorage.googleapis.com" ]
+}
+
+
 };
 
 export default nextConfig;
